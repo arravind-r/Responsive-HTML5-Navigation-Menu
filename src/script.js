08423bc43a48e511ff4fcf01bd57ecdc38911125
Arravind <aravindram024@gmail.com>
2021-02-26 20:12:33 +0530

    Fixed mobile menu toggle

--- src/script.js
+++ src/script.js
@@ -1,3 +1,9 @@
-function mobMenuToggle{
-    var mobMenuContainer = document.getElementsByClassName("mob-menu-container");
+var mobMenuContainer = document.getElementById("mobMenuCont");
+
+function mobMenuToggle() {    
+    mobMenuContainer.classList.add("displayon");
+}
+
+function closeMobMenuToggle() {
+    mobMenuContainer.classList.remove("displayon");
 }
