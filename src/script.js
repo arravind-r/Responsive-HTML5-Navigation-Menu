003d2d384c50d6b941556d84027b6d7ed1bfd4de
Arravind <aravindram024@gmail.com>
2021-02-26 20:21:26 +0530

    Optimized Mobile Menu html, css & js

--- src/script.js
+++ src/script.js
@@ -1,9 +1,12 @@
 var mobMenuContainer = document.getElementById("mobMenuCont");
+var mobMenuWrap = document.getElementById("mobMenuWrap");
 
 function mobMenuToggle() {    
     mobMenuContainer.classList.add("displayon");
+    mobMenuWrap.classList.add("displayon");
 }
 
 function closeMobMenuToggle() {
     mobMenuContainer.classList.remove("displayon");
+    mobMenuWrap.classList.remove("displayon");
 }
